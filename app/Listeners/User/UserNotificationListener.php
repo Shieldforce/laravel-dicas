a0a811c7e5a542bf41a4cfbb2209e2d6743fce8d
Alxandre Ferreira <shieldforce2@gmail.com>
2023-03-18 22:47:48 -0300

    conflit

---
+++ app/Listeners/User/UserNotificationListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Listeners\User;
+
+use App\Mail\UserStoreMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class UserNotificationListener
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(object $event): void
+    {
+        // $request = $event->request;
+        $user = $event->user;
+
+        Mail::to($user->email)->queue(new UserStoreMail($user));
+    }
+}
